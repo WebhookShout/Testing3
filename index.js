71927b7851708994f971c5b35e24dd5155087701
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-25 21:47:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const ScriptsLink = "";
 const ServiceKey = "AdRa-hXtp-44pk-uopl-cVIp-QdG1-Dnh1-adO0-russ-1ov3";
 
 //-- Encode Decode Word Function
@@ -49,54 +48,6 @@ function DecodeText(encoded, key) {
 }
 //--
 
-
-// Get Random Name Function
-function GetRandomName() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    const length = Math.floor(Math.random() * (32 - 16 + 1)) + 16; // random length between 16 and 32
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-
-// Generate Secured Key Function
-function generateSecureKey(length = 32, segmentLength = 4) {
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  const array = new Uint8Array(length);
-  crypto.getRandomValues(array);
-
-  let key = '';
-  for (let i = 0; i < array.length; i++) {
-    key += chars[array[i] % chars.length];
-    if ((i + 1) % segmentLength === 0 && i + 1 !== array.length) {
-      key += '-';
-    }
-  }
-  return key;
-}
-
-// Encode Ascii Function
-function encodeAscii(str) {
-  return str
-    .split('')
-    .map(char => char.charCodeAt(0))
-    .join('\\');
-}
-
-// Encode Script Function
-function EncodeScript(str, key) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(str); // UTF-8 bytes
-  const result = [];
-  for (let i = 0; i < data.length; i++) {
-    const k = key.charCodeAt(i % key.length);
-    result.push((data[i] + k) % 256); // still using key shift
-  }
-  return result.join('/');
-}
-
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
@@ -105,22 +56,7 @@ export default {
     const pathname = decodeURIComponent(url.pathname.slice(1)); // remove leading '/'
     const auth = url.searchParams.get("auth"); // get key in '?auth=Key'
 
-    // Detect if request UserAgent is not include "Roblox"
-    if (!userAgent.includes('Roblox')) {
-      return new Response('404: Not Found', { status: 403 });
-    }
-
-    let links = {};
-    try {
-      const jsonUrl = ScriptsLink;
-      const response = await fetch(jsonUrl);
-      if (!response.ok) throw new Error('Non-200 response');
-      links = await response.json();
-    } catch (e) {
-      return new Response("Failed to fetch JSON.", { status: 500 });
-    }
-
-    // Handle Access Scripts
+    // Handle Access
     if (pathname && auth) {
       const key = pathname;
       const linkData = links[key];
@@ -149,26 +85,7 @@ export default {
         headers: { "Content-Type": "text/plain" }
       });
     }
-
-    // Authorize Specific Key
-    if (pathname) {
-      const key = pathname;
-      const linkData = links[key];
-
-      if (!linkData) {
-        return new Response(`404: Not Found`, { status: 404 });
-      }
-
-      const randomName = GetRandomName();
-      const secureKey = generateSecureKey();
-      const json = JSON.stringify({Key: secureKey, Name: randomName, Expiration: Date.now() + 3000});
-      const code = `loadstring("\\${encodeAscii(`local t={[1]=Instance,[2]="new",[3]="StringValue",[4]=game,[5]="GetService",[6]="ReplicatedStorage",[7]="Parent",[8]="Name",[9]="Archivable",[10]="Value",[11]=true,[12]="${secureKey}",[13]="${randomName}"} local v=t[1][t[2]](t[3])v[t[7]]=t[4][t[5]](t[4], t[6])v[t[8]]=t[13]v[t[9]]=t[11]v[t[10]]=t[12] loadstring(game:HttpGet("${domain}/${url.pathname.slice(1)}?auth=${EncodeText(json, ServiceKey)}"))()`)}")()`;
-
-      return new Response(code, {
-        headers: { "Content-Type": "text/plain" }
-      });
-    }
-
+    
     return new Response("404: Not Found", { status: 404 });
   }
 }
